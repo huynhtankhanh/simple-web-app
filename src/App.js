cac4dca6ae0e5c0400cacf098c6a71bcf2cb72e7
Khanh Huynh <khanh.huynh@axonactive.com>
2020-03-29 22:55:41 +0700

    Fist commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Header from "./components/headerComponent/header.js";
+import Footer from "./components/footerComponent/footer.js";
+import Homepage from "./components/pages/homePage.js";
+import Products from "./components/pages/products.js";
+import Contact from "./components/pages/contact.js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {
+  BrowserRouter as Router,
+  Route
+} from "react-router-dom";
+
+import "./Assets/css/default.css";
+
+class App extends Component  {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+              <Header />
+
+              <Route exact path='/' component={Homepage}></Route>
+              <Route exact path='/Products' component={Products}></Route>
+              <Route exact path='/Contact' component={Contact}></Route>
+
+              <Footer />
+        </div>
+      </Router>  
+    );
+  }
 }
 
 export default App;
---
+++ src/components/footerComponent/footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'; 
+
+class Footer extends Component {
+    render() {
+       return ( 
+        <footer>
+            Test footer
+        </footer>
+       );
+    }
+}
+
+export default Footer;
